abd87cccb6b5e74b00a8e22b1083e53d9d38ccfb
Luka Cempre <luka@analyticspros.com>
2023-11-10 15:04:19 -0800

    Update traffic-source-query.sql

--- traffic-source-query.sql
+++ traffic-source-query.sql
@@ -2,9 +2,8 @@ DECLARE start_date STRING DEFAULT "20230723";
 DECLARE end_date STRING DEFAULT "20230723";
 
 #### OBSERVATIONS #### 
-#Entrances = 1 is unreliable, sometimes it shows up, sometimes it doesn't
-#Session_start has no sessions traffic source information
-#Going with the first non-null traffic source of the session could be wrong (since it could have started as Direct)
+#Session_start has sessions' traffic source information since the start of November 2023
+
 
 #### CONFIRM ####
 #What stream and events are exported in the BigQuery Linking in Admin
@@ -34,7 +33,7 @@ precedingSessions AS
       first_value(collected_traffic_source.manual_medium  IGNORE NULLS)  OVER (PARTITION BY user_pseudo_id, (SELECT value.int_value FROM unnest(event_params) WHERE key = 'ga_session_id') ORDER BY event_timestamp DESC) as old_medium,
       first_value(collected_traffic_source.manual_source  IGNORE NULLS)  OVER (PARTITION BY user_pseudo_id, (SELECT value.int_value FROM unnest(event_params) WHERE key = 'ga_session_id') ORDER BY event_timestamp DESC) as old_src,
       first_value(collected_traffic_source.gclid)  OVER (PARTITION BY user_pseudo_id, (SELECT value.int_value FROM unnest(event_params) WHERE key = 'ga_session_id') ORDER BY event_timestamp) as gclid
-      FROM `analytics_1234.events_*` WHERE event_name NOT IN ("session_start", "first_visit") AND _table_suffix BETWEEN FORMAT_DATE("%Y%m%d", DATE_SUB(PARSE_DATE("%Y%m%d", start_date), INTERVAL 90 DAY)) AND end_date)
+      FROM `analytics_1234.events_*` WHERE  _table_suffix BETWEEN FORMAT_DATE("%Y%m%d", DATE_SUB(PARSE_DATE("%Y%m%d", start_date), INTERVAL 90 DAY)) AND end_date)
 ),
 
 lastNonDirect AS (
